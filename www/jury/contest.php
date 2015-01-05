77c03fb80de224eff705c8f36030647fdd5263b6
Nicky Gerritsen <nickygerritsen@me.com>
2015-01-05 21:22:31 +0100

    Should be problem editor for us

--- www/jury/contest.php
+++ www/jury/contest.php
@@ -336,7 +336,7 @@ else {
 			  htmlspecialchars($row['color']) .
 			  ';"></div></a></td>'
 			: '<td>'. $link . '&nbsp;</a></td>' );
-		if ( IS_ADMIN ) echo "<td>" . delLinkMultiple('contestproblem',array('cid','probid'),array($id, $row['probid']), 'contest.php?id='.$id) ."</td>";
+		if ( IS_PROBLEM_EDITOR ) echo "<td>" . delLinkMultiple('contestproblem',array('cid','probid'),array($id, $row['probid']), 'contest.php?id='.$id) ."</td>";
 
 		$iseven = !$iseven;
 
